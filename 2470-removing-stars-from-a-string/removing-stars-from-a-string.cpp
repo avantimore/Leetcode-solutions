857cc5127ed9fc1e14f8c2a51229112c30e2a919
avantimore <108989035+avantimore@users.noreply.github.com>
2023-09-26 19:12:18 +0530

    Time: 86 ms (61.30%) | Memory: 27.3 MB (19.31%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -17,11 +17,46 @@ public:
             st.pop();
             tempans.push_back(stop);
         }
-        string ans="";
-        int m = tempans.length();
-        for(int i=m-1;i>=0;i--){
-            ans.push_back(tempans[i]);
-        }
-        return ans;
+            int m = tempans.length();
+            for (int i = 0; i < m / 2; i++) {
+                swap(tempans[i], tempans[m - i - 1]);
+            }
+        // string ans="";
+        // int m = tempans.length();
+        // for(int i=m-1;i>=0;i--){
+        //     ans.push_back(tempans[i]);
+        // }
+        return tempans;
     }
+// string removeStars(string s) {
+//     stack<char> st;
+//     int n = s.length();
+    
+//     for (int i = 0; i < n; i++) {
+//         if (s[i] >= 'a' && s[i] <= 'z') {
+//             st.push(s[i]);
+//         } else if (s[i] == '*') {
+//             if (!st.empty()) {
+//                 st.pop();
+//             }
+//         }
+//     }
+    
+//     string ans = "";
+    
+//     while (!st.empty()) {
+//         ans.push_back(st.top());
+//         st.pop();
+//     }
+    
+//     // Reverse the ans string in place
+//     int m = ans.length();
+//     for (int i = 0; i < m / 2; i++) {
+//         swap(ans[i], ans[m - i - 1]);
+//     }
+    
+//     return ans;
+// }
+
+
 };
