ba0a50747404ba4cd29e589137d9344be2caaf99
avantimore <108989035+avantimore@users.noreply.github.com>
2023-09-28 07:25:49 +0530

    Time: 20 ms (41.23%) | Memory: 19.6 MB (13.86%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int s=0;
+        int mid=0;
+        int e=n-1;
+        while(mid<=e){
+            if(nums[mid]!=0){
+                swap(nums[s],nums[mid]);
+                s++;
+            }
+            mid++;
+        }
+    }
+};
