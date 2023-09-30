bf904cc1a8df74b09c6cfd8a07b684427cc4cd6c
avantimore <108989035+avantimore@users.noreply.github.com>
2023-09-30 11:10:34 +0530

    Time: 12 ms (90.63%) | Memory: 31.4 MB (39.91%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        long long backSum = 0;
-        int n = nums.size();
-        for(int i=1;i<n;i++){
-            backSum +=nums[i];
-        }
-        if(backSum==0){
-            return 0;
-        }
-        long long frontSum = nums[0];
-        for(int i=1;i<n;i++){
+        // long long backSum = 0;
+        // int n = nums.size();
+        // for(int i=1;i<n;i++){
+        //     backSum +=nums[i];
+        // }
+        // if(backSum==0){
+        //     return 0;
+        // }
+        // long long frontSum = nums[0];
+        // for(int i=1;i<n;i++){
             
-            backSum -= nums[i];
-            if(backSum==frontSum){
+        //     backSum -= nums[i];
+        //     if(backSum==frontSum){
+        //         return i;
+        //     }
+        //     frontSum +=nums[i];
+        // }
+        // if(frontSum-nums[n-1]==0){
+        //     return n-1;
+        // }
+        // return -1;
+
+        int totalWeightOnLeft = 0;
+        int totalWeightOnRight = std::accumulate( nums.begin(), nums.end(), 0);
+        
+        
+        for(std::size_t i = 0; i < nums.size() ; i++ ){
+            
+            int currentWeight = nums[i];
+            
+            totalWeightOnRight -= currentWeight;
+            
+            if( totalWeightOnLeft == totalWeightOnRight ){
+                // balance is met on both sides
                 return i;
             }
-            frontSum +=nums[i];
+            
+            totalWeightOnLeft += currentWeight;
         }
-        if(frontSum-nums[n-1]==0){
-            return n-1;
-        }
+        
+        
         return -1;
     }
 };
