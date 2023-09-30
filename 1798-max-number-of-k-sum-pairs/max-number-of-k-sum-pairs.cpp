e3ef67b02b371e2dea3875982a2a6b2b66fb194a
avantimore <108989035+avantimore@users.noreply.github.com>
2023-09-30 08:54:34 +0530

    Time: 87 ms (94.27%) | Memory: 58.4 MB (52.17%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        // sort(nums.begin(),nums.end());
-        // int i=0;
-        // int j=nums.size()-1;
-        // int count=0;
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=nums.size()-1;
+        int count=0;
 
-        // while(i<j){
-        //     if(nums[i]+nums[j]==k){
-        //         count++;
-        //         i++;
-        //         j--;
-        //     }
-        //     else if(nums[i]+nums[j]<k){
-        //         i++;
-        //     }
-        //     else{
-        //         j--;
-        //     }
-        // }
-        // return count;
-
-        unordered_map<int, int> m;
-		int ans = 0;
-		for(int i = 0; i < nums.size(); i++){
-			if(m[k - nums[i]] > 0){
-			   m[k - nums[i]]--;
-				ans++;
-			}
-			else{
-				m[nums[i]]++;
-			}
-		}
-		return ans;
+        while(i<j){
+            if(nums[i]+nums[j]==k){
+                count++;
+                i++;
+                j--;
+            }
+            else if(nums[i]+nums[j]<k){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return count;
+        
     }
 };
