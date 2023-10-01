62ddeeb78dbd5feac69d24af79f23f13cf6fab1c
avantimore <108989035+avantimore@users.noreply.github.com>
2023-10-01 17:50:44 +0530

    Time: 133 ms (67.38%) | Memory: 110 MB (9.29%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -1,29 +1,33 @@
- #include<algorithm>
+
+#include<algorithm>
 class Solution {
-   
+    
 public:
+    int max(int i,int j){
+        if(i<j)return j;
+        return i;
+    }
     double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
         double maxAvg = INT_MIN;
         double currSum = 0;
-        int n = nums.size();
         for(int i=0;i<k;i++){
             currSum += nums[i];
         }
-        
-        if(maxAvg > currSum/k){
-            maxAvg = maxAvg;
-        }
-        else{
-            maxAvg = currSum/k;
-        }
+        if (maxAvg > currSum / k) {
+    maxAvg = maxAvg;
+} else {
+    maxAvg = currSum / k;
+}
+
         for(int i=1;i<=n-k;i++){
             currSum = currSum - nums[i-1] + nums[i+k-1];
-            if(maxAvg > currSum/k){
-                maxAvg = maxAvg;
-            }
-            else{
-                maxAvg = currSum/k;
-            }
+            if (maxAvg > currSum / k) {
+    maxAvg = maxAvg;
+} else {
+    maxAvg = currSum / k;
+}
+
         }
         return maxAvg;
     }
