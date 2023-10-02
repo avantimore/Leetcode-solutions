4ba2eca225ea8879d2b3ce6e49c6cb5eac49d5ae
avantimore <108989035+avantimore@users.noreply.github.com>
2023-10-02 11:54:34 +0530

    Time: 64 ms (78.11%) | Memory: 59.4 MB (7.46%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        // int left = 0;
-        // int right = height.size() - 1;
-        // int maxArea = 0;
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
 
-        // while (left < right) {
-        //     int currentArea = min(height[left], height[right]) * (right - left);
-        //     maxArea = max(maxArea, currentArea);
+        while (left < right) {
+            int currentArea = min(height[left], height[right]) * (right - left);
+            maxArea = max(maxArea, currentArea);
 
-        //     if (height[left] < height[right]) {
-        //         left++;
-        //     } else {
-        //         right--;
-        //     }
-        // }
-
-        // return maxArea;
-
-        int start = 0, end = height.size()-1;
-        int max_ans = INT_MIN;
-        while(start < end) {
-            int curr_ans = (end-start)*min(height[start], height[end]);
-            max_ans = max(curr_ans, max_ans);
-            if(height[start] < height[end])
-                start++;
-            else
-                end--;
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
         }
-        return max_ans;
+
+        return maxArea;
     }
 };
