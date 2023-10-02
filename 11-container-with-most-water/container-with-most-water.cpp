df1584164373d6a1b366d5eb62362d727cc44ce9
avantimore <108989035+avantimore@users.noreply.github.com>
2023-10-02 11:53:53 +0530

    Time: 69 ms (55.68%) | Memory: 59.3 MB (25.65%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int left = 0;
-        int right = height.size() - 1;
-        int maxArea = 0;
+        // int left = 0;
+        // int right = height.size() - 1;
+        // int maxArea = 0;
 
-        while (left < right) {
-            int currentArea = min(height[left], height[right]) * (right - left);
-            maxArea = max(maxArea, currentArea);
+        // while (left < right) {
+        //     int currentArea = min(height[left], height[right]) * (right - left);
+        //     maxArea = max(maxArea, currentArea);
 
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
+        //     if (height[left] < height[right]) {
+        //         left++;
+        //     } else {
+        //         right--;
+        //     }
+        // }
+
+        // return maxArea;
+
+        int start = 0, end = height.size()-1;
+        int max_ans = INT_MIN;
+        while(start < end) {
+            int curr_ans = (end-start)*min(height[start], height[end]);
+            max_ans = max(curr_ans, max_ans);
+            if(height[start] < height[end])
+                start++;
+            else
+                end--;
         }
-
-        return maxArea;
+        return max_ans;
     }
 };
