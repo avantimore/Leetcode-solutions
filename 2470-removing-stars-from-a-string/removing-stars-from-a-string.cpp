30308a5a7dfea8831628d591ef249ab2c598d82f
avantimore <108989035+avantimore@users.noreply.github.com>
2023-10-03 18:44:12 +0530

    Time: 77 ms (88.84%) | Memory: 24.5 MB (92.10%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,62 +1,14 @@
 class Solution {
 public:
     string removeStars(string s) {
-        string tempans="";
-        stack<char> st;
-        int n = s.length();
-        for(int i=0;i<n;i++){
-            if(s[i]>='a' && s[i]<='z'){
-                st.push(s[i]);
-            }
-            else if(s[i]=='*'){
-                st.pop();
+        int i=0,j=0;
+        for(i=0;i<s.size();i++){
+            if(s[i]=='*'){
+                j--;
+            }else{
+                s[j++] = s[i];
             }
         }
-        while(!st.empty()){
-            char stop = st.top();
-            st.pop();
-            tempans.push_back(stop);
-        }
-            int m = tempans.length();
-            for (int i = 0; i < m / 2; i++) {
-                swap(tempans[i], tempans[m - i - 1]);
-            }
-        // string ans="";
-        // int m = tempans.length();
-        // for(int i=m-1;i>=0;i--){
-        //     ans.push_back(tempans[i]);
-        // }
-        return tempans;
+        return s.substr(0,j);
     }
-// string removeStars(string s) {
-//     stack<char> st;
-//     int n = s.length();
-    
-//     for (int i = 0; i < n; i++) {
-//         if (s[i] >= 'a' && s[i] <= 'z') {
-//             st.push(s[i]);
-//         } else if (s[i] == '*') {
-//             if (!st.empty()) {
-//                 st.pop();
-//             }
-//         }
-//     }
-    
-//     string ans = "";
-    
-//     while (!st.empty()) {
-//         ans.push_back(st.top());
-//         st.pop();
-//     }
-    
-//     // Reverse the ans string in place
-//     int m = ans.length();
-//     for (int i = 0; i < m / 2; i++) {
-//         swap(ans[i], ans[m - i - 1]);
-//     }
-    
-//     return ans;
-// }
-
-
 };
