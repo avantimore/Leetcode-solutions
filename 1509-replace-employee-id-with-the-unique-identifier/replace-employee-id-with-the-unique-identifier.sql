d5f049b3fee97d2dd6dda11c06d7638c49832cad
avantimore <108989035+avantimore@users.noreply.github.com>
2023-11-11 10:56:28 +0530

    Time: 2571 ms (24.21%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,8 @@
-# Write your MySQL query statement below
-select unique_id,name from Employees left outer join EmployeeUNI
-on Employees.id = EmployeeUNI.id;
+SELECT
+    (SELECT EmployeeUNI.unique_id
+     FROM EmployeeUNI
+     WHERE Employees.id = EmployeeUNI.id) AS unique_id,
+    Employees.name
+FROM Employees;
+
+
