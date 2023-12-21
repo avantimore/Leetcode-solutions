ac0da93c1082faadb82ed05a87afe51eb5a0b605
avantimore <108989035+avantimore@users.noreply.github.com>
2023-12-21 18:15:49 +0530

    Time: 15 ms (19.68%) | Memory: 6.3 MB (39.78%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int actualNum = x;
-        if(x<0)return 0;
-
-        long long num = 0;
-        while(x>0){
+        int org = x;
+        if(x<0) return 0;
+        long num=0;
+        while (x > 0) {
             int digit = x % 10;
-            num = num * 10+digit;
-            x = x/10;
+            num = num * 10 + digit;
+            x = x / 10;
         }
-        if(num==actualNum)return 1;
+        if(num==org)return 1;
         return 0;
     }
 };
