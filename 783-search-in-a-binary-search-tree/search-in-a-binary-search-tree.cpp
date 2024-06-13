a43b33e23101850507c2e1b7510153237f131ed7
avantimore <108989035+avantimore@users.noreply.github.com>
2024-06-13 07:40:32 +0530

    Time: 37 ms (12.83%) | Memory: 33 MB (73.47%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -11,23 +11,15 @@
  */
 class Solution {
 public:
-    
     TreeNode* searchBST(TreeNode* root, int val) {
-         while (root != NULL && root->val != val) {
-
-        // If the current node's value is greater than the target value,
-        // move to the left subtree to continue the search.
-        if (root->val > val)
-            root = root->left;
-
-        // If the current node's value is lesser than the target value,
-        // move to the right subtree to continue the search.
-        else
-            root = root->right;
-    }
-
-    // Return the current node (which contains the target value) if found,
-    // or NULL if the target value is not present in the BST.
-    return root;
+        if(root==NULL)return NULL;
+        if(root->val==val)return root;
+        else if(root->val < val){
+            return searchBST(root->right, val);
+        }
+        // else{
+            return searchBST(root->left, val);
+        // }
+        // return
     }
 };
