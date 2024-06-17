8c133747a94b0a6591f9667a77c1d5c17d12e051
avantimore <108989035+avantimore@users.noreply.github.com>
2024-06-17 16:20:08 +0530

    Time: 91 ms (6.19%) | Memory: 70.1 MB (92.70%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // int maxSum = 0;
-        // int sum = nums[0];
-        // if(nums.size()==1)return nums[0];
+        int maxSum = nums[0];
+        int sum = 0;
 
-        // for(int i=1;i<nums.size();i++){
-        //     sum+=nums[i];
-        //     maxSum = max(maxSum,sum);
-        //     if(sum<0){
-        //         sum = 0;
-        //     }
-        // }
-        // return maxSum;
-
-
-
-        int maxi=nums[0];
-        int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            maxi=max(sum,maxi);
+            maxSum = max(maxSum,sum);
             if(sum<0){
-                sum=0;
+                sum = 0;
             }
-            
         }
-        return maxi;
+        return maxSum;
+
     }
 };
