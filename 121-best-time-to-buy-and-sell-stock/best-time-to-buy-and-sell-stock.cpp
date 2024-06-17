437352e9d8d9163f90ee6aa495dc2e06ab5b0aeb
avantimore <108989035+avantimore@users.noreply.github.com>
2024-06-17 15:46:15 +0530

    Time: 87 ms (52.17%) | Memory: 95.7 MB (93.50%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+
         int greater = 0;
         int n = prices.size();
+        int maxProf = 0;
 
         for(int i=n-1;i>=0;i--){
             if(prices[i]<greater){
-                prices[i] = greater - prices[i];
+                maxProf = max(maxProf,greater - prices[i]);
             }
             else{
                 greater = prices[i];
@@ -14,10 +16,6 @@ public:
             }
         }
 
-        int maxProf = INT_MIN;
-        for(int i=0;i<n;i++){
-            maxProf = max(maxProf,prices[i]);
-        }
         return maxProf;
     }
 };
