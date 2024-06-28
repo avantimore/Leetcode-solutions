01ec820e361dae1f4177f83cf657e07cbb2f1351
avantimore <108989035+avantimore@users.noreply.github.com>
2024-06-28 19:11:27 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (47.41%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int n) {
+    vector<int> generateRow(int row){
+        vector<int> temp;
+        temp.push_back(1);
+        long long ans = 1;
+        for(int col=1;col<row;col++){
+            ans *= (row-col);
+            ans /= col;
+            temp.push_back(ans);
+        }
+        return temp;
+    }
+    vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        ans.push_back({1});
-        if(n==1)return ans;
-        ans.push_back({1,1});
-        for(int i=2;i<n;i++){
-            vector<int> temp;
-            temp.push_back(1);
-            int sz = ans.size();
-            for(int j=0;j<ans[sz-1].size()-1;j++){
-                int sum = ans[sz-1][j]+ans[sz-1][j+1];
-                temp.push_back(sum);
-            }
-            temp.push_back(1);
-            ans.push_back(temp);
+        for(int i=1;i<=numRows;i++){
+            ans.push_back(generateRow(i));
         }
         return ans;
     }
