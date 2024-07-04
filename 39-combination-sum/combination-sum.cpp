3852b3493fbc3414fa1477df6598780c2d6023b7
avantimore <108989035+avantimore@users.noreply.github.com>
2024-07-04 21:01:41 +0530

    Time: 3 ms (80.68%) | Memory: 12.3 MB (65.41%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    void solve(vector<int>& nums,vector<int> &temp,vector<vector<int>> &ans,int i,int sum,int target){
+        
+        if(i>=nums.size() || sum>target)return;
+        if(sum==target){
+            ans.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[i]);
+        solve(nums,temp,ans,i,sum+nums[i],target);
+        temp.pop_back();
+        solve(nums,temp,ans,i+1,sum,target);
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int> temp;
+        int sum= 0;
+
+        solve(nums,temp,ans,0,sum,target);
+        return ans;
+    }
+};
