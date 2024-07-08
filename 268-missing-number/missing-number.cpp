77a6820605b92bd59aa23ca987964566c6cd63ac
avantimore <108989035+avantimore@users.noreply.github.com>
2024-07-08 19:51:33 +0530

    Time: 15 ms (52.03%) | Memory: 20.3 MB (85.05%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int actual = n*(n+1)/2;
-        int currSum =0;
-        for(int i=0;i<n;i++){
-            currSum += nums[i];
+        int idealSum = n*(n+1)/2;   
+
+        int sum = 0;
+        for(int &i:nums){
+            sum += i;
         }
-        return (actual-currSum);
+        return (idealSum-sum);
     }
 };
