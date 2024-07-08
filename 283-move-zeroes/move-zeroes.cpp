136bb4bc54c7da8f0df871d1bc307dc47689de49
avantimore <108989035+avantimore@users.noreply.github.com>
2024-07-08 19:17:02 +0530

    Time: 19 ms (37.53%) | Memory: 21.5 MB (95.18%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
-   void moveZeroes(vector<int>& nums) {
-   
-        for(int i=0,next=0;i<nums.size();i++)
-            if(nums[i])
-                swap(nums[i],nums[next++]);
-    }
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        int j = 0;
+        int zero = 0;
+        int n = nums.size();
 
+        while(i<n && j<n){
+            if(nums[j]==0){
+                j++;
+            }
+            else{
+                nums[i++] = nums[j++];
+            }
+        }
+
+        while(i<n){
+            nums[i++] = 0;
+        }
+    }
 };
