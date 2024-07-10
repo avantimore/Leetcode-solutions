3e846fa63744112c14742a966f4d1dc0208b25fd
avantimore <108989035+avantimore@users.noreply.github.com>
2024-07-10 14:38:29 +0530

    Time: 21 ms (41.01%) | Memory: 24.7 MB (14.15%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -14,22 +14,13 @@ public:
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }
-            if(mid%2==0){
-                if(nums[mid]==nums[mid+1]){
-                    s = mid+1;
-                }
-                else{
-                    e = mid-1;
-                }
+            if ((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2!=0 && nums[mid]!=nums[mid+1])){
+                s = mid+1;
             }
             else{
-                if(nums[mid]==nums[mid+1]){
-                    e = mid-1;
-                }
-                else{
-                    s = mid+1;     
-                }
+                e = mid-1;
             }
+            
         }
         return -1;
     }
