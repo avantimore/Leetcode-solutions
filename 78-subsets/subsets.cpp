8a9dc5d8673f091fcceb2c88a0655e83253bb6d2
avantimore <108989035+avantimore@users.noreply.github.com>
2024-07-11 18:12:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (45.65%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     void solve(vector<vector<int>>& ans,vector<int> &temp,vector<int>& nums,int i,int n){
-        ans.push_back(temp);
-        // if(i>=n)return;
-        for(int j=i;j<n;j++){
-            temp.push_back(nums[j]);
-            solve(ans,temp,nums,j+1,n);
-            temp.pop_back();
+        
+        if(i>=n){
+            ans.push_back(temp);
+            return;
         }
-        // solve(ans,temp,nums,i+1,n);
+        temp.push_back(nums[i]);
+        solve(ans,temp,nums,i+1,n);
+        temp.pop_back();
+        solve(ans,temp,nums,i+1,n);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
