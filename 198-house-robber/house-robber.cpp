df5e268aba114ded8ae3da21c12c77f9e6c2502d
avantimore <108989035+avantimore@users.noreply.github.com>
2024-07-15 21:27:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (5.96%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -15,7 +15,7 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
 
-        if(n==1)return nums[0];
+        // if(n==1)return nums[0];
         // if(n==2) return max(nums[0],nums[1]);
         
         // for(int i=2;i<n;i++){
