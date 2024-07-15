815aff5ac7ae33a93598d5ba36eb957d673d9e9b
avantimore <108989035+avantimore@users.noreply.github.com>
2024-07-15 21:23:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (5.96%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(int i,vector<int>& nums,vector<int> &dp){
+        if(i==0){
+            return dp[0] = nums[0];
+        }
+        if(i<0)return 0;
+        
+        if(dp[i]!=-1) return dp[i];
+        int take = nums[i] + solve(i-2,nums,dp);
+        int notTake = solve(i-1,nums,dp);
+
+        return dp[i] = max(take,notTake);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n==1)return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+
+        vector<int> dp(n,-1);
+        return solve(n-1,nums,dp);
+    }
+};
