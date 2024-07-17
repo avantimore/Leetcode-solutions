df052ea03ef94c08707ddf9a06ee39fcb7189d9a
avantimore <108989035+avantimore@users.noreply.github.com>
2024-07-17 22:15:46 +0530

    Time: 16 ms (37.84%) | Memory: 26.8 MB (19.16%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -34,12 +34,13 @@ public:
         int i=0;
         int e = inorder.size()-1;
         int s = 0;
-        int num = preorder[i++];
-        TreeNode* root = new TreeNode(num);
-        int ind = find(inorder,num,e+1);
+        // int num = preorder[i++];
+        // TreeNode* root = new TreeNode(num);
+        // int ind = find(inorder,num,e+1);
 
-        root->left = build(i,s,ind-1,preorder,inorder,e+1);
-        root->right = build(i,ind+1,e,preorder,inorder,e+1);
+        // root->left = 
+        TreeNode* root = build(i,s,e,preorder,inorder,e+1);
+        // root->right = build(i,ind+1,e,preorder,inorder,e+1);
 
         return root;
     }
