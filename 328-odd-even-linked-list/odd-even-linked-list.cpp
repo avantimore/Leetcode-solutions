8d5411819dc45725c1b1b8a3289aeadf8e3d00d7
avantimore <108989035+avantimore@users.noreply.github.com>
2024-07-18 14:16:52 +0530

    Time: 4 ms (90.40%) | Memory: 15.3 MB (8.68%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,19 +11,20 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(!head || !head->next || !head->next->next) return head;
-        
-        ListNode *odd = head;
-        ListNode *even = head->next;
-        ListNode *even_start = head->next;
-        
-        while(odd->next && even->next){
-            odd->next = even->next; //Connect all odds
-            even->next = odd->next->next;  //Connect all evens
+        if (!head || !head->next) return head;
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;  // Save the head of the even list to connect later
+
+        while (even && even->next) {
+            odd->next = even->next;
             odd = odd->next;
+            even->next = odd->next;
             even = even->next;
         }
-        odd->next = even_start;   //Place the first even node after the last odd node.
-        return head; 
+
+        odd->next = evenHead;  // Connect the end of the odd list to the head of the even list
+        return head;
     }
 };
