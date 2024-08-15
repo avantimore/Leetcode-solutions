fab514386572b17b83c00ded3db60f08fb40c382
avantimore <108989035+avantimore@users.noreply.github.com>
2024-08-15 10:11:00 +0530

    Time: 14 ms (76.11%) | Memory: 17.1 MB (51.90%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int stops) {
-    vector<vector<pair<int, int>>> gph(n);
-    for (const auto& flight : flights) {
-        int u = flight[0];
-        int v = flight[1];
-        int d = flight[2];
-        gph[u].push_back({v, d});
-    }
-
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
-    pq.push({0, {src, 0}});
-    
-    // Use a vector to store the minimum cost to reach each node with at most k stops
-    vector<vector<int>> dist(n, vector<int>(stops + 2, INT_MAX));
-    dist[src][0] = 0;
-
-    while (!pq.empty()) {
-        auto [cost, pos] = pq.top();
-        pq.pop();
-        int node = pos.first;
-        int k = pos.second;
-
-        if (node == dst) return cost;
-
-        if (k > stops) continue;
-
-        for (const auto& [neighbor, price] : gph[node]) {
-            int new_cost = cost + price;
-            if (new_cost < dist[neighbor][k + 1]) {
-                dist[neighbor][k + 1] = new_cost;
-                pq.push({new_cost, {neighbor, k + 1}});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+         vector<pair<int,int>>adj[n];
+        for(auto it:flights)
+        {
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        queue<pair<int,pair<int,int>>>q;
+            // {stops, {node, distance}}
+        q.push({0,{src,0}});
+        vector<int>dist(n,INT_MAX);
+        dist[src]=0;
+        while(!q.empty())
+        {
+            auto it=q.front();
+            q.pop();
+            int stops=it.first;
+            int node=it.second.first;
+            int weight=it.second.second;
+            if(stops>K)
+                continue;
+            for(auto i:adj[node])
+            {
+                int adjNode=i.first;
+                int adjWt=i.second;
+                if(weight+adjWt<dist[adjNode])
+                {
+                    q.push({stops+1,{adjNode,weight+adjWt}});
+                    dist[adjNode]=weight+adjWt;
+                }
             }
         }
+            // destination do not reached under k stops
+        if(dist[dst]==INT_MAX)
+            return -1;
+        return dist[dst];
     }
-    return -1;
-}
 };
